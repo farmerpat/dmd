a8fc6d4a4088ce75d52a153ba5b3141e6fc02846
Patrick Connelly <patricksparbanieconnelly@gmail.com>
2017-10-31 00:46:29 -0400

    init spells seeder

---
+++ database/migrations/2017_10_30_144005_create_spells_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSpellsTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('spells', function (Blueprint $table) {
+            $table->integer('id');
+            //$table->increments('id');
+            //$table->int('index');
+            $table->string('name');
+            $table->longText('desc');
+            $table->longText('higher_level');
+            $table->string('page');
+            $table->string('range');
+            $table->string('components');
+            $table->text('material');
+            $table->boolean('ritual');
+            $table->string('duration');
+            $table->boolean('concentration');
+            $table->string('casting_time');
+            $table->integer('level');
+            // we probably want this to be an id that keys on school table id
+            $table->string('school');
+            // instead of this being a csv of class strings, we probably
+            // want (in addition to a class table), and junction table
+            // call spell_classes or something that can have multiple
+            // entries for each spell, where each entry is spell id (or) index and
+            // the id of the class it belongs to
+            $table->string('classes');
+            // as above
+            $table->string('subclasses');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('spells');
+    }
+}
---
+++ database/seeds/SpellsTableSeeder.php
@@ -0,0 +1,168 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SpellsTableSeeder extends Seeder {
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() {
+        \DB::table('spells')->truncate();
+        \DB::table('spells')->insert(
+            $this->generateDataArray()
+
+        );
+    }
+
+    // should I be using a factory instead?
+    // or an abstract factory that creates
+    // a factory that points to a specific file?
+    // that would be possible if the first line
+    // of the file specified the structure of the
+    // rest of the json file
+    // it looks like laravels factories are for generating
+    // data (the only use case i've seen is for generating
+    // test data). I think using them to feed the seeder
+    // with permenant data is in contrast to the spirit
+    // of their intended use.
+    // the abstact in my case would be a seederFactory instead
+    // it could take the table name and/or spec and the model somehow
+    // and the filename that contains the seed data
+    public function generateDataArray () {
+        $entries = [];
+        $fname = __dir__ . '/../../util/jsonfiles/spells.json';
+        $handle = fopen($fname, 'r');
+
+        if ($handle) {
+            while (($line = fgets($handle)) !== false) {
+                $thisEntry = [];
+
+                $json = json_decode($line, true);
+                var_dump( $json );
+
+                if ($json) {
+                    $thisEntry = $this->generateEntryFromJson($json);
+
+                    $entries[] = $thisEntry;
+                }
+            }
+
+            fclose($handle);
+        }
+
+        return $entries;
+    }
+
+    private function generateEntryFromJson ($json) {
+        $thisEntry = [];
+
+        $thisEntry['id'] = $json['index'];
+        $thisEntry['name'] = $json['name'];
+
+        $desc = $json['desc'];
+
+        if (is_array($desc) && (count($desc) > 0)) {
+            $desc = implode(';', $desc);
+        } else {
+            $desc = 'unknown';
+        }
+
+        $thisEntry['desc'] = $desc;
+
+        if (array_key_exists('higher_level', $json)) {
+            $higherLevel = $json['higher_level'];
+
+            if (is_array($higherLevel) && (count($higherLevel) > 0)) {
+                $higherLevel = implode(';', $higherLevel);
+
+            } else {
+                $higherLevel = '';
+
+            }
+        } else {
+            $higherLevel = '';
+
+        }
+
+        $thisEntry['higher_level'] = $higherLevel;
+
+        $thisEntry['page'] = $json['page'];
+        $thisEntry['range'] = $json['range'];
+
+        $components = $json['components'];
+
+        if (is_array($components) && (count($components) > 0)) {
+            $components = implode(';', $components);
+        } else {
+            $components = 'unknown';
+        }
+
+        $thisEntry['components'] = $components;
+
+        if (array_key_exists('material', $json)) {
+            $thisEntry['material'] = $json['material'];
+        } else {
+            $thisEntry['material'] = 'none';
+        }
+
+        $thisEntry['ritual'] = ($json['ritual'] == 'yes') ? true : false;
+        $thisEntry['duration'] = $json['duration'];
+        $thisEntry['concentration'] = ($json['concentration'] == 'yes') ? true : false;
+        $thisEntry['casting_time'] = $json['casting_time'];
+        $thisEntry['level'] = $json['level'];
+
+        $school = $json['school'];
+
+        if (is_array($school) && array_key_exists('name', $school)) {
+            $school = $school['name'];
+        } else {
+            $school = 'unknown';
+        }
+
+        $thisEntry['school'] = $school;
+
+        $classes = $json['classes'];
+
+        if (is_array($classes) && (count($classes) > 0)) {
+            $classNames = [];
+
+            foreach ($classes as $class) {
+                if (array_key_exists('name', $class)) {
+                    $classNames[] = $class['name'];
+                }
+            }
+
+            $classes = implode(';', $classNames);
+        } else {
+            $classes = 'unknown';
+        }
+
+        $thisEntry['classes'] = $classes;
+
+        $subClasses = $json['subclasses'];
+
+        if (is_array($subClasses) && (count($subClasses) > 0)) {
+            $subClassNames = [];
+
+            foreach ($subClasses as $subClass) {
+                if (array_key_exists('name', $subClass)) {
+                    $subClassNames[] = $subClass['name'];
+                }
+            }
+
+            $subClasses = implode(';', $subClassNames);
+
+        } else {
+            $subClasses = '';
+        }
+
+        $thisEntry['subclasses'] = $subClasses;
+
+        $thisEntry['created_at'] = DB::raw('now()');
+        $thisEntry['updated_at'] = DB::raw('now()');
+
+        return $thisEntry;
+    }
+}
